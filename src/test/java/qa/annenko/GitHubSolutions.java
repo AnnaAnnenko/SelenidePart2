49f33a178ce40e06c955e8ad3448baf4937a37ec
User <annenko.anya@gmail.com>
2022-11-22 12:45:35 +0300

    Add new test

--- src/test/java/qa/annenko/GitHubSolutions.java
+++ src/test/java/qa/annenko/GitHubSolutions.java
@@ -1,4 +1,20 @@
 package qa.annenko;
 
+import com.codeborne.selenide.Condition;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+
 public class GitHubSolutions {
+
+    String BaseURL = "https://github.com/";
+
+    @Test
+    public void checkNameOfPage() {
+        open(BaseURL);
+        $(byText("Solutions")).hover();
+        $(byText("Enterprise")).click();
+        $(".h1-mktg.mb-3").shouldHave(Condition.exactTextCaseSensitive("Build like the best"));
+    }
 }
